fc8754d06185f8d7bfe4fd8b13c857e52e326ab0
DivallMedia <divallmedia@gmail.com>
2014-02-12 17:19:58 +0000

    Stonehearth

--- Stonehearth/src/com/mime/stonehearth/stonehearth_display.java
+++ Stonehearth/src/com/mime/stonehearth/stonehearth_display.java
@@ -1,11 +1,28 @@
 package com.mime.stonehearth;
 
-
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 public class stonehearth_display {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.setTitle("Stonehearth Pre-Alpha 0.0.1");
+            Display.create();
+        } catch (LWJGLException e) {
+            System.err.println("Display wasn't initialized correctly.");
+            System.exit(1);
+        }
+ 
+        while (!Display.isCloseRequested()) {
+            Display.update();
+            Display.sync(60);
+        }
+ 
+        Display.destroy();
+        System.exit(0);
 
 	}
 
